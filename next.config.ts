4e53ccea5f56bfc1e3b0652476782e0e3934d6c3
David <yugiohgxd600@gmail.com>
2025-04-18 15:24:00 -0700

    run deploy

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // If using <Image> from next/image
+  },
+  trailingSlash: true, // Needed for GitHub Pages
+  basePath: '/GOE', // Replace with your repo name
+};
 
 export default nextConfig;
